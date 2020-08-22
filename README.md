1bcb62c14fd9179a8cfff1e362bd0a239164391c
Tobias Blass <tblass@t-blass.de>
2020-08-22 20:10:29 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # websitediff
+
+A small utility to check websites for changes.
+
+## Usage
+
+The tool expects a list of URLs in its configuration file (`$XDG_CONFIG_HOME/websitediff.conf`, defaults to `~/.config/websitediff.conf`) and checks all of them for modifications since the last time the tool ran. URLs can alternatively be passed on the command line; the configuration file is ignored in this case.
+
+By default, the tool just prints the URLs that have changed. Modify the `print_diff` variable to get a textual diff.
+
+## Ideas
+
+- Output an HTML rendering of the website with differences marked
---
+++ websitediff.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import requests
+from pathlib import Path
+from os import environ
+import difflib
+from urllib.parse import urlparse
+import time
+import sys
+
+cache_dir = Path(environ.get('XDG_CACHE_HOME', Path.home()/".cache"), "websitediff")
+config_file = Path(environ.get('XDG_CONFIG_HOME', Path.home()/".config"), "websitediff.conf")
+
+#Change this variable to print the website diff in textual form. Disabled by default
+print_diff = False
+
+def website_cache_file(url):
+    _,loc,path,*ignore = urlparse(url)
+    if path and path[0] == '/':
+        path = path[1:]
+    return cache_dir/loc/path
+    
+def diff_website(url, store_new=True):
+    diff = None
+    req = requests.get(url)
+    req.raise_for_status()
+
+    cache_file = website_cache_file(url)
+    cache_file.parent.mkdir(parents=True, exist_ok=True)
+    if cache_file.exists():
+        diff = difflib.unified_diff(cache_file.read_text().split('\n'),
+                                    req.text.split('\n'),
+                                    fromfile=url,
+                                    tofile=url,
+                                    lineterm='',
+                                    fromfiledate=time.ctime(cache_file.stat().st_mtime),
+                                    tofiledate=time.ctime())
+
+
+    if store_new:
+        cache_file.write_text(req.text)
+    
+    return diff
+
+def main(urls=None):
+    if urls is None:
+        with config_file.open() as config:
+            urls = list(config)
+    for url in urls:
+        if 'http' not in url:
+            url = f'https://{url}'
+        lines = diff_website(url)
+        if lines:
+            if print_diff:
+                print('\n'.join(lines))
+            else:
+                print(url)
+
+if __name__ == '__main__':
+    cmdline_urls = ([arg for arg in sys.argv[1:] if arg and arg[0] != '-'] or
+                    None)
+
+    main(urls=cmdline_urls)
